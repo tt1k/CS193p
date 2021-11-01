506b9b73763f2d793f72d0dc2613c6f3a4d75395
FakeCoder <icedotaku.dev@gmail.com>
2021-11-02 00:10:33 +0800

    Lecture 10: Multithreading Demo Gestures

--- Memorize/EmojiArt/EmojiArtDocumentView.swift
+++ Memorize/EmojiArt/EmojiArtDocumentView.swift
@@ -23,18 +23,29 @@ struct EmojiArtDocumentView: View {
     var documentBody: some View {
         GeometryReader { geometry in
             ZStack {
-                Color.yellow
-                ForEach(document.emojis) { emoji in
-                    Text(emoji.text)
-                        .font(.system(size: fontSize(of: emoji)))
-                        .position(position(of: emoji, in: geometry))
+                Color.white.overlay(
+                    OptionalImage(uiImage: document.backgroundImage)
+                        .scaleEffect(zoomScale)
+                        .position(convertFromEmojiCoordinates((0, 0), in: geometry))
+                )
+                .gesture(doubleTapToZoom(in: geometry.size))
+                if document.backgroundImageFetchStatus == .fetching {
+                    ProgressView().scaleEffect(2.0)
+                } else {
+                    ForEach(document.emojis) { emoji in
+                        Text(emoji.text)
+                            .font(.system(size: fontSize(of: emoji)))
+                            .scaleEffect(zoomScale)
+                            .position(position(of: emoji, in: geometry))
+                    }
                 }
             }
-            .onDrop(of: [.plainText], isTargeted: nil) { providers, location in
+            .clipped()
+            .onDrop(of: [.plainText, .url, .image], isTargeted: nil) { providers, location in
                 drop(providers: providers, location: location, in: geometry)
             }
+            .gesture(panGesture().simultaneously(with: zoomGesture()))
         }
-
     }
     
     var palette: some View {
@@ -54,8 +65,8 @@ struct EmojiArtDocumentView: View {
     private func convertFromEmojiCoordinates(_ location: (x: Int, y: Int), in geometry: GeometryProxy) -> CGPoint {
         let center = geometry.frame(in: .local).center
         return CGPoint(
-            x: center.x + CGFloat(location.x),
-            y: center.y + CGFloat(location.y)
+            x: center.x + CGFloat(location.x) * zoomScale + panOffset.width,
+            y: center.y + CGFloat(location.y) * zoomScale + panOffset.height
         )
     }
     
@@ -63,22 +74,91 @@ struct EmojiArtDocumentView: View {
     private func convertToEmojiCoordinates(_ location: CGPoint, in geometry: GeometryProxy) -> (x: Int, y: Int) {
         let center = geometry.frame(in: .local).center
         let location = CGPoint(
-            x: location.x - center.x,
-            y: location.y - center.y
+            x: (location.x - panOffset.width - center.x) / zoomScale,
+            y: (location.y - panOffset.height - center.y) / zoomScale
         )
         return (x: Int(location.x), y: Int(location.y))
     }
     
     /// handle user droping an emoji
     private func drop(providers: [NSItemProvider], location: CGPoint, in geometry: GeometryProxy) -> Bool {
-        return providers.loadObjects(ofType: String.self) { string in
-            if let emoji = string.first, emoji.isEmoji {
-                document.addEmoji(
-                    String(string),
-                    at: convertToEmojiCoordinates(location, in: geometry),
-                    size: defaultEmojiFontSize)
+        var found = providers.loadObjects(ofType: URL.self) { url in
+            document.setBackground(EmojiArtModel.Background.url(url.imageURL))
+        }
+        if !found {
+            found = providers.loadObjects(ofType: UIImage.self) { image in
+                if let data = image.jpegData(compressionQuality: 1.0) {
+                    document.setBackground(EmojiArtModel.Background.imageData(data))
+                }
             }
         }
+        if !found {
+            found = providers.loadObjects(ofType: String.self) { string in
+                if let emoji = string.first, emoji.isEmoji {
+                    document.addEmoji(
+                        String(string),
+                        at: convertToEmojiCoordinates(location, in: geometry),
+                        size: defaultEmojiFontSize / zoomScale)
+                }
+            }
+        }
+        return found
+    }
+    
+    @State private var steadyStateZoomScale: CGFloat = 1.0
+    @GestureState private var gestureZoomScale: CGFloat = 1.0
+    
+    private var zoomScale: CGFloat {
+        steadyStateZoomScale * gestureZoomScale
+    }
+    
+    private func zoomToFit(_ image: UIImage?, in size: CGSize) {
+        if let image = image, image.size.width > 0, image.size.height > 0, size.width > 0, size.height > 0 {
+            let hZoom = size.width / image.size.width
+            let vZoom = size.height / image.size.height
+            steadyStatePanOffset = .zero
+            steadyStateZoomScale = min(hZoom, vZoom)
+        }
+    }
+    
+    /// double tap to make image fill the screen
+    private func doubleTapToZoom(in size: CGSize) -> some Gesture {
+        TapGesture(count: 2)
+            .onEnded {
+                withAnimation {
+                    zoomToFit(document.backgroundImage, in: size)
+                }
+            }
+    }
+    
+    /// zoom image with two fingers
+    private func zoomGesture() -> some Gesture {
+        MagnificationGesture()
+            .updating($gestureZoomScale) { latestGestureScale, gestureZoomScale, _ in
+                gestureZoomScale = latestGestureScale
+            }
+            .onEnded { gestureScaleAtEnd in
+                steadyStateZoomScale *= gestureScaleAtEnd
+            }
+    }
+    
+    @State private var steadyStatePanOffset: CGSize = .zero
+    @GestureState private var gesturePanOffset: CGSize = .zero
+    
+    private var panOffset: CGSize {
+        /// reload CGSize + operator
+        (steadyStatePanOffset + gesturePanOffset) * zoomScale
+    }
+    
+    /// drag the whole view
+    private func panGesture() -> some Gesture {
+        DragGesture()
+            .updating($gesturePanOffset) { latestDragGestureValue, gesturePanOffset, _ in
+                gesturePanOffset = latestDragGestureValue.translation / zoomScale
+            }
+            .onEnded { finalDragGesture in
+                steadyStatePanOffset = steadyStatePanOffset + (finalDragGesture.translation / zoomScale)
+            }
     }
 }
 
--- Memorize/EmojiArt/EmojiArtDocumentViewModel.swift
+++ Memorize/EmojiArt/EmojiArtDocumentViewModel.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 class EmojiArtDocumentViewModel: ObservableObject {
-    @Published private(set) var emojiArt: EmojiArtModel
+    @Published private(set) var emojiArt: EmojiArtModel {
+        didSet {
+            if emojiArt.background != oldValue.background {
+                fetchBackgroundImageDataIfNecessary()
+            }
+        }
+    }
     
     init() {
         emojiArt = EmojiArtModel()
@@ -18,6 +24,38 @@ class EmojiArtDocumentViewModel: ObservableObject {
     var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
     
     var background: EmojiArtModel.Background { emojiArt.background }
+    
+    @Published var backgroundImage: UIImage?
+    @Published var backgroundImageFetchStatus = BackgroundImageFetchStatus.idle
+    
+    enum BackgroundImageFetchStatus {
+        case idle
+        case fetching
+    }
+    
+    private func fetchBackgroundImageDataIfNecessary() {
+        backgroundImage = nil
+        switch emojiArt.background {
+            case .url(let url):
+                /// fetch the url
+                backgroundImageFetchStatus = .fetching
+                DispatchQueue.global(qos: .userInitiated).async {
+                    let imageData = try? Data(contentsOf: url)
+                    DispatchQueue.main.async { [weak self] in
+                        if self?.emojiArt.background == EmojiArtModel.Background.url(url) {
+                            self?.backgroundImageFetchStatus = .idle
+                            if imageData != nil {
+                                self?.backgroundImage = UIImage(data: imageData!)
+                            }
+                        }
+                    }
+                }
+            case .imageData(let data):
+                backgroundImage = UIImage(data: data)
+            case .blank:
+                break
+        }
+    }
     
     /// MARK: Intent
     
